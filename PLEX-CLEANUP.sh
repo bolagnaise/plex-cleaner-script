0c69607f98831901b074655c9ee9d72c34c44933
bolagnaise <bolagnaise@msn.com>
2023-09-23 22:57:32 +1000

    Update PLEX-CLEANUP.sh

--- PLEX-CLEANUP.sh
+++ PLEX-CLEANUP.sh
@@ -1,124 +1,68 @@
-#!/bin/bash
+##!/bin/bash
 
-####### EDIT ONLY THESE SETTINGS #######
+# Root directory where your TV shows and Movies folders are located
+root_directory="/mnt/user0/rclone_upload/gdrive_vfs/"
 
-# Full directory paths (update these paths as needed)
-tv_shows_directory="/mnt/user/rclone_upload/gdrive_vfs/TV Shows"
-movies_directory="/mnt/user/rclone_upload/gdrive_vfs/Movies"
-#fourk_tv_shows_directory="/mnt/user/rclone_upload/gdrive_vfs/4KTVShows" # Uncomment if required
-#fourk_movies_directory="/mnt/user/rclone_upload/gdrive_vfs/4KMovies" # Uncomment if required
+# Maximum space for the root directory in megabytes
+max_space_mb=30000000  # Adjust this value as needed #default is 30TB
 
-# Default minimum free space (2 TiB)
-min_free_space=2048  # 2 TiB (1 TiB = 1024 GiB, multiple 1024 x your desired min free space in TiB ie 3 x 1024 = 3072)
-
-# Default time periods in days for each directory
-tv_shows_time_period=30
-movies_time_period=60
-#fourk_tv_shows_time_period=90 # Uncomment if required
-#fourk_movies_time_period=120 # Uncomment if required
-
-# Set this to true for a dry run (no files will be deleted)
+# Dry run flag (set to true to simulate, set to false to actually delete files)
 dry_run=true
 
-####### END SETTINGS #######
+################################################################
+# DO NOT EDIT BELOW THIS LINE
+###############################################################
 
-###############################################################################
-#####   DO NOT EDIT ANYTHING BELOW UNLESS YOU KNOW WHAT YOU ARE DOING   #######
-###############################################################################
+# Function to calculate the total size of a directory in megabytes
+calculate_directory_size_mb() {
+    du -sm "$1" | awk '{print $1}'
+}
 
-# Function to delete old files, prioritizing the oldest ones
-delete_old_files() {
-    local directory="$1"
-    local required_min_free_space_gib="$2"
+# Calculate the total size of the root directory in megabytes
+total_size_mb=$(calculate_directory_size_mb "$root_directory")
+echo "Total size of root directory: ${total_size_mb}MB"
 
-    if [ ! -d "$directory" ]; then
-        echo "Directory $directory does not exist. Skipping..."
-        return
-    fi
+# Calculate the free space in the root directory in megabytes using du
+free_space_mb=$(calculate_directory_size_mb "$root_directory")
+echo "Free space in root directory: ${free_space_mb}MB"
 
-    if [ "$dry_run" = true ]; then
-        echo "Dry run: Would have deleted files in $directory to free up space."
+# Check if the current size of the root directory exceeds the maximum allowed size
+if [ "$total_size_mb" -gt "$max_space_mb" ]; then
+    space_to_free_mb=$((total_size_mb - max_space_mb))
+
+    echo "Current size: ${total_size_mb}MB"
+    echo "Maximum allowed size: ${max_space_mb}MB"
+
+    # Check if space to free is greater than 0 (meaning we need to delete files)
+    if [ "$space_to_free_mb" -gt 0 ]; then
+        echo "Starting file deletion..."
+        if [ "$dry_run" = true ]; then
+            # Dry run: Print the files that would be deleted
+            find "$root_directory" -type f -exec echo "Would delete: {}" \;
+        else
+            # Actual run: Delete the oldest files until enough space is freed
+            while [ "$space_to_free_mb" -gt 0 ]; do
+                # Find and delete the oldest file
+                oldest_file=$(find "$root_directory" -type f -print0 | xargs -0 ls -lt | awk -v SPACE_TO_FREE="$space_to_free_mb" '
+                    {
+                        space_to_free_mb -= $5 / (1024*1024);  # Convert file size to MB
+                        if (space_to_free_mb >= 0) {
+                            print $9;
+                        }
+                    }
+                ' | head -n 1)
+                if [ -z "$oldest_file" ]; then
+                    break  # No more files to delete
+                fi
+                rm -f "$oldest_file"
+                space_to_free_mb=$((space_to_free_mb - $(ls -l "$oldest_file" | awk '{print $5 / (1024*1024)}')))
+                echo "Deleted: $oldest_file"
+            done
+        fi
+        echo "Finished cleaning. Free space in root directory: ${free_space_mb}MB"
     else
-        # Find and delete the oldest files until the required minimum free space is met
-        while [ $(df -BG "$(dirname "$directory")" | awk 'NR==2 {print $4}' | tr -d 'G') -lt "$required_min_free_space_gib" ]; do
-            # Find the oldest file in the directory
-            oldest_file=$(find "$directory" -type f -printf '%T+ %p\n' 2>/dev/null | sort | head -n 1 | awk '{print $NF}')
-            
-            # Check if there are no more files to delete
-            if [ -z "$oldest_file" ]; then
-                echo "No more files to delete in $directory. Free space is still below the required limit."
-                break
-            fi
-
-            # Delete the oldest file
-            if [ -f "$oldest_file" ]; then
-                file_size_gib=$(du -sk --apparent-size "$oldest_file" | awk '{print $1 / 1024 / 1024}')
-                required_min_free_space_gib=$((required_min_free_space_gib - file_size_gib))
-                rm "$oldest_file"
-                echo "Deleted file: $oldest_file"
-            else
-                echo "File $oldest_file no longer exists. Skipping..."
-            fi
-        done
-        
-        echo "Deleted old files in $directory to free up space."
+        echo "No files to delete in the root directory. Free space in root directory: ${free_space_mb}MB"
     fi
-}
-
-# Function to get the directory size in gibibytes as a floating-point number
-get_directory_size() {
-    local directory="$1"
-    if [ ! -d "$directory" ]; then
-        echo "Directory $directory does not exist. Skipping..."
-        return 0
-    fi
-    du -sk --apparent-size "$directory" | awk '{print $1 / 1024 / 1024}'
-}
-
-main() {
-    # Use provided or default paths and options
-    local tv_shows_directory="${1:-$tv_shows_directory}"
-    local movies_directory="${2:-$movies_directory}"
-    local fourk_tv_shows_directory="${3:-$fourk_tv_shows_directory}"
-    local fourk_movies_directory="${4:-$fourk_movies_directory}"
-
-    current_tv_shows_size=$(get_directory_size "$tv_shows_directory")
-    current_movies_size=$(get_directory_size "$movies_directory")
-    current_fourk_tv_shows_size=$(get_directory_size "$fourk_tv_shows_directory")
-    current_fourk_movies_size=$(get_directory_size "$fourk_movies_directory")
-
-    current_free_space=$(df -BG "$(dirname "$tv_shows_directory")" | awk 'NR==2 {print $4}' | tr -d 'G')
-
-    # Convert the minimum free space requirement to GiB
-    required_min_free_space_gib=$((min_free_space * 1024))
-
-    if [ "$current_free_space" -lt "$required_min_free_space_gib" ]; then
-        echo "Free space in $(dirname "$tv_shows_directory") is below the required limit. Checking time period..."
-        
-        # Display the calculated free space
-        echo "Calculated free space: $current_free_space GiB"
-
-        echo "Deleting old files..."
-        if [ -n "$tv_shows_directory" ]; then
-            delete_old_files "$tv_shows_directory" "$required_min_free_space_gib"
-        fi
-        if [ -n "$movies_directory" ]; then
-            delete_old_files "$movies_directory" "$required_min_free_space_gib"
-        fi
-        if [ -n "$fourk_tv_shows_directory" ]; then
-            delete_old_files "$fourk_tv_shows_directory" "$required_min_free_space_gib"
-        fi
-        if [ -n "$fourk_movies_directory" ]; then
-            delete_old_files "$fourk_movies_directory" "$required_min_free_space_gib"
-        fi
-    else
-        echo "Free space in $(dirname "$tv_shows_directory") is sufficient. No action needed."
-        # Display the calculated free space
-        echo "Calculated free space: $current_free_space GiB"
-    fi
-
-    echo "Cleanup process completed."
-}
-
-# Pass directory paths as arguments or use defaults
-main "$@"
+else
+    echo "No action needed for the root directory. Free space in root directory: ${free_space_mb}MB"
+fi
